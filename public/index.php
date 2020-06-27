1457d575b2c28965346e6b4ef5038cdde1bc342a
Kaleb <kalebcooper@gmail.com>
2020-06-27 14:56:33 +1200

    Setup SCSS and minor styling

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Cash Money Consultants</title>
+    </head>
+    <body>
+        <h1>New Website Coming Soon</h1>
+    </body>
+</html>
---
+++ public/staff/index.php
@@ -0,0 +1,29 @@
+<?php 
+date_default_timezone_set('NZ-CHAT');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>CMC</title>
+        <link rel="stylesheet" media="all" href="../styles/styles.css" />
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    </head>
+    <body id="staff-page">
+        <header>
+            <h1>CMC Dashboard</h1>
+        </header>
+        <div id="side-bar">
+            <nav>
+                <ul>
+                    <li><span class="material-icons">home</span><a href="index.php">Home</a></li>
+                </ul>
+            </nav>
+        </div>
+        <footer>
+            &copy; <?php print date("Y"); ?> Website designed by Kaleb Cooper.
+        </footer>
+
+
+    </body>
+</html>
